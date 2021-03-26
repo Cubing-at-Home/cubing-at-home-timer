e5082cee9befd11e964bd8a83e5c15a36e10b8b4
Oscar Di Lenarda <oscar@dilenarda.com>
2021-03-26 17:47:11 +0100

    Switched to environment variable for WCA secret

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
+  apiKey: process.env.REACT_APP_FB_API_KEY,
   authDomain: 'cubing-at-home.firebaseapp.com',
   databaseURL: 'https://cubing-at-home.firebaseio.com',
   projectId: 'cubing-at-home',
   storageBucket: 'cubing-at-home.appspot.com',
   messagingSenderId: '1053566413406',
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
